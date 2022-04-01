d615c8c16948dde537d27336fd68b3fbdb5ae101
NRehberg-Axonom <65667480+NRehberg-Axonom@users.noreply.github.com>
2022-04-01 10:59:33 -0500

    initial add everything

---
+++ Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace AsciiArt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.tbWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb11 = new System.Windows.Forms.TextBox();
+            this.tb10 = new System.Windows.Forms.TextBox();
+            this.tb9 = new System.Windows.Forms.TextBox();
+            this.tb8 = new System.Windows.Forms.TextBox();
+            this.tb7 = new System.Windows.Forms.TextBox();
+            this.tb6 = new System.Windows.Forms.TextBox();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.tb12 = new System.Windows.Forms.TextBox();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button1.Location = new System.Drawing.Point(87, 332);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select image to turn into ascii art!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.listBox1.Location = new System.Drawing.Point(53, 117);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(85, 52);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 84);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ascii Art Size";
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(275, 113);
+            this.tbHeight.Margin = new System.Windows.Forms.Padding(4);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(84, 22);
+            this.tbHeight.TabIndex = 3;
+            // 
+            // tbWidth
+            // 
+            this.tbWidth.Location = new System.Drawing.Point(275, 145);
+            this.tbWidth.Margin = new System.Windows.Forms.Padding(4);
+            this.tbWidth.Name = "tbWidth";
+            this.tbWidth.Size = new System.Drawing.Size(84, 22);
+            this.tbWidth.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 117);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 149);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(196, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Number of pixels per character";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(156, 209);
+            this.tbTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(264, 22);
+            this.tbTitle.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 213);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ".txt Title:";
+            // 
+            // tb11
+            // 
+            this.tb11.Location = new System.Drawing.Point(524, 113);
+            this.tb11.Margin = new System.Windows.Forms.Padding(4);
+            this.tb11.Name = "tb11";
+            this.tb11.Size = new System.Drawing.Size(64, 22);
+            this.tb11.TabIndex = 10;
+            // 
+            // tb10
+            // 
+            this.tb10.Location = new System.Drawing.Point(524, 145);
+            this.tb10.Margin = new System.Windows.Forms.Padding(4);
+            this.tb10.Name = "tb10";
+            this.tb10.Size = new System.Drawing.Size(64, 22);
+            this.tb10.TabIndex = 11;
+            // 
+            // tb9
+            // 
+            this.tb9.Location = new System.Drawing.Point(524, 177);
+            this.tb9.Margin = new System.Windows.Forms.Padding(4);
+            this.tb9.Name = "tb9";
+            this.tb9.Size = new System.Drawing.Size(64, 22);
+            this.tb9.TabIndex = 12;
+            // 
+            // tb8
+            // 
+            this.tb8.Location = new System.Drawing.Point(524, 209);
+            this.tb8.Margin = new System.Windows.Forms.Padding(4);
+            this.tb8.Name = "tb8";
+            this.tb8.Size = new System.Drawing.Size(64, 22);
+            this.tb8.TabIndex = 13;
+            // 
+            // tb7
+            // 
+            this.tb7.Location = new System.Drawing.Point(524, 241);
+            this.tb7.Margin = new System.Windows.Forms.Padding(4);
+            this.tb7.Name = "tb7";
+            this.tb7.Size = new System.Drawing.Size(64, 22);
+            this.tb7.TabIndex = 14;
+            // 
+            // tb6
+            // 
+            this.tb6.Location = new System.Drawing.Point(524, 273);
+            this.tb6.Margin = new System.Windows.Forms.Padding(4);
+            this.tb6.Name = "tb6";
+            this.tb6.Size = new System.Drawing.Size(64, 22);
+            this.tb6.TabIndex = 15;
+            // 
+            // tb5
+            // 
+            this.tb5.Location = new System.Drawing.Point(524, 305);
+            this.tb5.Margin = new System.Windows.Forms.Padding(4);
+            this.tb5.Name = "tb5";
+            this.tb5.Size = new System.Drawing.Size(64, 22);
+            this.tb5.TabIndex = 16;
+            // 
+            // tb12
+            // 
+            this.tb12.Location = new System.Drawing.Point(524, 80);
+            this.tb12.Margin = new System.Windows.Forms.Padding(4);
+            this.tb12.Name = "tb12";
+            this.tb12.Size = new System.Drawing.Size(64, 22);
+            this.tb12.TabIndex = 17;
+            // 
+            // tb4
+            // 
+            this.tb4.Location = new System.Drawing.Point(524, 337);
+            this.tb4.Margin = new System.Windows.Forms.Padding(4);
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(64, 22);
+            this.tb4.TabIndex = 18;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(524, 369);
+            this.tb3.Margin = new System.Windows.Forms.Padding(4);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(64, 22);
+            this.tb3.TabIndex = 19;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(524, 401);
+            this.tb2.Margin = new System.Windows.Forms.Padding(4);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(64, 22);
+            this.tb2.TabIndex = 20;
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(524, 433);
+            this.tb1.Margin = new System.Windows.Forms.Padding(4);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(64, 22);
+            this.tb1.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(456, 84);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Darkest";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(456, 437);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 17);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Whitest";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 28);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Restore Default Chars";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(460, 54);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 17);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Characters To Use";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(156, 254);
+            this.tbFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(264, 22);
+            this.tbFolder.TabIndex = 26;
+            this.tbFolder.Click += new System.EventHandler(this.tbFolder_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 257);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 17);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Folder To Save To:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Lucida Console", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(1, 15);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 7);
+            this.label10.TabIndex = 28;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(53, 25);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 17);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "Ascii Art Creator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 484);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb4);
+            this.Controls.Add(this.tb12);
+            this.Controls.Add(this.tb5);
+            this.Controls.Add(this.tb6);
+            this.Controls.Add(this.tb7);
+            this.Controls.Add(this.tb8);
+            this.Controls.Add(this.tb9);
+            this.Controls.Add(this.tb10);
+            this.Controls.Add(this.tb11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbWidth);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Ascii Art Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.TextBox tbWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb11;
+        private System.Windows.Forms.TextBox tb10;
+        private System.Windows.Forms.TextBox tb9;
+        private System.Windows.Forms.TextBox tb8;
+        private System.Windows.Forms.TextBox tb7;
+        private System.Windows.Forms.TextBox tb6;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.TextBox tb12;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AsciiArt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SelectedIndex = 0;
+            tbHeight.Text = "10";
+            tbWidth.Text = "8";
+            RestoreDefaultChars();
+            tbFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op1 = new OpenFileDialog();
+            op1.Multiselect = false;
+            DialogResult result = op1.ShowDialog();
+            if (result != DialogResult.Cancel)
+            {
+                op1.Filter = "allfiles|*.png";
+                string[] FName;
+                string newFilePath, newFileName;
+                int height = int.Parse(tbHeight.Text);
+                int width = int.Parse(tbWidth.Text);
+                foreach (string s in op1.FileNames)
+                {
+                    Image image1 = Image.FromFile(s);
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        FName = s.Split('\\');
+                        newFilePath = tbFolder.Text.Trim().Length > 0 ? tbFolder.Text : Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        newFileName = tbTitle.Text.Trim().Length > 0 ? tbTitle.Text.Replace(".txt", "") + ".txt" : FName[FName.Length - 1].Replace(".png", ".txt").Replace(".jpg", ".txt").Replace(".jpeg", ".txt");
+                        using (StreamWriter file = new StreamWriter(newFilePath + "\\" + newFileName))
+                        {
+                            image1.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+                            Bitmap bm = new Bitmap(stream);
+                            Color origColor;
+                            int colorAve;
+                            for (int i = 0; i < bm.Height / height; i++)
+                            {
+                                for (int j = 0; j < bm.Width / width; j++)
+                                {
+                                    colorAve = 0;
+                                    for (int k = 0; k < height; k++)
+                                    {
+                                        for (int l = 0; l < width; l++)
+                                        {
+                                            origColor = bm.GetPixel(j * width + l, i * height + k);
+                                            int grayScale = (int)((origColor.R * .3) + (origColor.G * .59) + (origColor.B * .11));
+                                            colorAve += grayScale;
+                                        }
+                                    }
+                                    colorAve = Convert.ToInt32(colorAve / (height * width));
+                                    if (j == (bm.Width / width) - 1)
+                                        file.WriteLine(GetAsciiChar(colorAve));
+                                    else
+                                        file.Write(GetAsciiChar(colorAve));
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Image has been artified!");
+            }
+        }
+
+        private string GetAsciiChar(int grayScale)
+        {
+            if (grayScale < 10)
+                return tb12.Text.Trim().Length > 0 ? tb12.Text.Substring(0, 1) : " ";
+            else if (grayScale < 25)
+                return tb11.Text.Trim().Length > 0 ? tb11.Text.Substring(0, 1) : " ";
+            else if (grayScale < 40)
+                return tb10.Text.Trim().Length > 0 ? tb10.Text.Substring(0, 1) : " ";
+            else if (grayScale < 60)
+                return tb9.Text.Trim().Length > 0 ? tb9.Text.Substring(0, 1) : " ";
+            else if (grayScale < 80)
+                return tb8.Text.Trim().Length > 0 ? tb8.Text.Substring(0, 1) : " ";
+            else if (grayScale < 100)
+                return tb7.Text.Trim().Length > 0 ? tb7.Text.Substring(0, 1) : " ";
+            else if (grayScale < 125)
+                return tb6.Text.Trim().Length > 0 ? tb6.Text.Substring(0, 1) : " ";
+            else if (grayScale < 150)
+                return tb5.Text.Trim().Length > 0 ? tb5.Text.Substring(0, 1) : " ";
+            else if (grayScale < 175)
+                return tb4.Text.Trim().Length > 0 ? tb4.Text.Substring(0, 1) : " ";
+            else if (grayScale < 200)
+                return tb3.Text.Trim().Length > 0 ? tb3.Text.Substring(0, 1) : " ";
+            else if (grayScale < 225)
+                return tb2.Text.Trim().Length > 0 ? tb2.Text.Substring(0, 1) : " ";
+            else
+                return tb1.Text.Trim().Length > 0 ? tb1.Text.Substring(0, 1) : " ";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+            {
+                tbHeight.Text = "10";
+                tbWidth.Text = "8";
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                tbHeight.Text = "6";
+                tbWidth.Text = "4";
+            }
+            else
+            {
+                tbHeight.Text = "3";
+                tbWidth.Text = "2";
+            }
+        }
+
+        private void tbHeight_TextChanged(object sender, EventArgs e)
+        {
+            int height;
+            if (!int.TryParse(tbHeight.Text, out height))
+                tbHeight.Text = "8";
+        }
+
+        private void tbWidth_TextChanged(object sender, EventArgs e)
+        {
+            int width;
+            if (!int.TryParse(tbWidth.Text, out width))
+                tbWidth.Text = "6";
+        }
+
+        private void tbFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.Description = "Select folder to save ascii art to.";
+            folderBrowser.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderBrowser.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if(folderBrowser.ShowDialog() != DialogResult.Cancel)
+                tbFolder.Text = folderBrowser.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RestoreDefaultChars();
+        }
+
+        private void RestoreDefaultChars()
+        {
+            tb12.Text = "K";
+            tb11.Text = "N";
+            tb10.Text = "#";
+            tb9.Text = "@";
+            tb8.Text = "&";
+            tb7.Text = "%";
+            tb6.Text = "i";
+            tb5.Text = "c";
+            tb4.Text = ":";
+            tb3.Text = "*";
+            tb2.Text = ".";
+            tb1.Text = " ";
+        }
+    }
+}
